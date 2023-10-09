47ee5df1f5fa6179ee05bde54becbe50ee3bd774
Jefferson <jefferson.brandao@g.globo>
2023-10-09 20:36:28 -0300

    criando stubs de criptografia

---
+++ test/cryptography/fake-hasher.ts
@@ -0,0 +1,12 @@
+import { HasherCompare } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HasherGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+export class FakeHasher implements HasherGenerator, HasherCompare {
+  async hash(plain: string): Promise<string> {
+    return plain.concat('-hashed')
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return plain.concat('-hashed') === hash
+  }
+}
